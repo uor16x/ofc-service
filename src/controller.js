c28e705015310858eff3f44fd455c0fa1f235b5f
uor16x <u0r.home@gmail.com>
2022-05-13 11:56:45 +0300

    Calc POST endpoint init

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,25 @@
+const { StatusCodes } = require('http-status-codes')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+
 module.exports = {
-  async calc(_request, _reply) {
-    return []
+  async calc(request, reply) {
+    const input = request.body
+    if (!process.env.CALC_PATH) {
+      throw new Error('Please provide CALC_PATH environment variable')
+    }
+    let result
+    try {
+      const arg = input
+        .map(({ username, cards }) => `(\\"${username}\\", [${cards.map(card => `\\"${card}\\"`)}])`)
+        .join(',')
+      const { stdout } = await exec(`${process.env.CALC_PATH} "[${arg}]"`)
+      result = JSON.parse(JSON.parse(stdout)) // external calc binary requires double unpacking
+    } catch (err) {
+      return reply
+        .status(StatusCodes.BAD_REQUEST)
+        .send(new Error(`Failed to parse: ${err.stderr}`))
+    }
+    return result
   }
 }
