dafaab8e0414e9f66cdf9df591e35e44987116f1
Mangix <forprxy@gmail.com>
2022-05-25 01:26:19 +0300

    Project files restructuring

--- src/controllers/game.js
+++ src/controllers/game.js
@@ -1,30 +1,8 @@
 const { StatusCodes } = require('http-status-codes')
-const util = require('util')
-const exec = util.promisify(require('child_process').exec)
 const mongoose = require('mongoose')
-const { gameSchema } = require('./schemas/game')
+const { gameSchema } = require('../schemas/game')
 
 module.exports = {
-  async calc(request, reply) {
-    const input = request.body
-    if (!process.env.CALC_PATH) {
-      throw new Error('Please provide CALC_PATH environment variable')
-    }
-    let result
-    try {
-      const arg = input
-        .map(({ username, cards }) => `(\\"${username}\\", [${cards.map(card => `\\"${card}\\"`)}])`)
-        .join(',')
-      const { stdout } = await exec(`${process.env.CALC_PATH} "[${arg}]"`)
-      result = JSON.parse(JSON.parse(stdout)) // external calc binary requires double unpacking
-    } catch (err) {
-      return reply
-        .status(StatusCodes.BAD_REQUEST)
-        .send(new Error(`Failed to parse: ${err.stderr}`))
-    }
-    return result
-  },
-
   async createGame(request) {
     const body = request.body
     const Game = mongoose.model('Game', gameSchema)
---
+++ src/routes/calculation.js
@@ -0,0 +1,55 @@
+const { auth } = require('../middlewares')
+const { StatusCodes } = require('http-status-codes')
+const { calc } = require('../controllers/calculation')
+
+module.exports = () => {
+  const app = require('../app')()
+
+  app.route({
+    method: 'POST',
+    url: '/calc',
+    schema: {
+      body: {
+        type: 'array',
+        maxItems: 3,
+        minItems: 3,
+        items: {
+          type: 'object',
+          required: ['username', 'cards'],
+          properties: {
+            username: { type: 'string' },
+            cards: {
+              type: 'array',
+              minItems: 13,
+              maxItems: 13,
+              items: {
+                type: 'string',
+                maxLength: 2,
+                minLength: 2
+              }
+            }
+          }
+        },
+      },
+      response: {
+        [StatusCodes.OK]: {
+          type: 'array',
+          maxItems: 3,
+          minItems: 3,
+          items: {
+            type: 'object',
+            required: ['username', 'result'],
+            properties: {
+              username: { type: 'string' },
+              result: {
+                type: 'object'
+              }
+            }
+          }
+        }
+      }
+    },
+    preHandler: auth,
+    handler: calc
+  })
+}
